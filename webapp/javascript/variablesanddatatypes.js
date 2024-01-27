7ac30c73a7f6cb48569b5a83b393bd5a46bdb803
saily <sailypande15@gmail.com>
2024-01-27 22:55:06 +0530

    add files

---
+++ webapp/javascript/variablesanddatatypes.js
@@ -0,0 +1,17 @@
+console.log("Welcome to my javascript file...");
+let var1="this is latest variable";
+const var2 = "this is constant value";
+var var3 = "this is legacy variable";
+
+console.log("var1 = "+var1);
+console.log("var2 = "+var2);
+console.log("var1 = "+var3);
+
+
+var1="new val1";
+//var2 ="new val 2";
+var3 = "new val3";
+
+onsole.log("var1 = "+var1);
+console.log("var2 = "+var2);
+console.log("var1 = "+var3);
